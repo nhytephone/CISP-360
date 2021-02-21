4c74c739faced193e30e3de8f96a25b0b594c105
nhytephone <60491609+nhytephone@users.noreply.github.com>
2021-02-21 10:09:37 -0800

    Update hw5_Sum.cpp
    first attempt

--- week_5/paired/hw5_Sum.cpp
+++ week_5/paired/hw5_Sum.cpp
@@ -11,3 +11,40 @@ Program description: A program that asks the user for a positive integer value.
 The program then uses a loop to get the sum of all the integers from 1 up to
 the number entered
 *****************************************************/
+#include <iostream>
+#include <chrono>
+using namespace std;
+void response_sanity(int &response);
+
+int main()
+{
+  auto begin = std::chrono::high_resolution_clock::now();
+  int response;
+  int sum = 0;
+  cout << "Enter a positive integer value: ";
+  cin >> response;
+  response_sanity(response);
+  for (int i = 0; i <= response; i++)
+  {
+    sum += i;
+  }
+  cout << endl
+       << "The sum of the numbers 1 - " << response << " is " << sum << "."
+       << endl << endl
+       << "----------------------------------" << endl;
+
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  printf("Process exited after %.1f seconds with return value 0", elapsed.count() * 1e-9);
+}
+
+void response_sanity(int &response)
+{
+  while (response < 0 )
+  {
+    cout << endl << "The starting number must be positive." << endl
+         << "Enter a positive integer value: ";
+    cin >> response;
+  }
+}
